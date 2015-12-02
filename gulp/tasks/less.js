1839ee731dd0b57eced9801ee7c653360d282ad9
devmetal <devmetal91@gmail.com>
2015-12-02 16:49:59 +0100

    Clean up project

--- gulp/tasks/less.js
+++ gulp/tasks/less.js
@@ -6,12 +6,12 @@ let util = require('gulp-util');
 let browserSync = require('browser-sync');
 
 gulp.task('less', function(){
-  return gulp.src('app/less/main.less')
+  return gulp.src('public/stylesheets/style.less')
     .pipe(less())
     .on('error', function(err){
       util.log(util.color.red('Error'), err.message);
       this.emit('end');
     })
-    .pipe(gulp.dest('./public/styles/'))
+    .pipe(gulp.dest('./public/stylesheets/'))
     .pipe(browserSync.reload({stream:true, one:true}));
 });
